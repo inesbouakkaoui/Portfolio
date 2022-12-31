0d4682a67ac85c884a3a3345a36219e2fc90c678
Ines <ines.bouakkaoui@gmail.com>
2022-12-31 17:58:01 +0100

    hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
-import "./navbarstyles.css"
+import "./NavbarStyles.css"
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 
+import { FaBars, FaTimes } from "react-icons/fa"
+
 const Navbar = () => {
+// making menu active
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
   return (
     <div className="header">
         <Link to="/">
             <h1 className="main-title">Portfolio.</h1>
         </Link>
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li>
                 <Link to="/">Home</Link>
             </li>
@@ -23,6 +30,9 @@ const Navbar = () => {
                 <Link to="/contact">Contact</Link>
             </li>
         </ul>
+        <div className="hamburger" onClick={handleClick}>
+            {click ? (<FaTimes size={20} style={{ color: "#fff"}} />) : <FaBars size={20} style={{ color: "#fff"}} />}
+        </div>
          </div>
   )
 }
