4950fe2b5cdf34e5d1012b635412ffc4f67f71df
Ines <ines.bouakkaoui@gmail.com>
2023-01-01 23:35:06 +0100

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import "./FooterStyles.css"
+
+import React from 'react'
+import { FaHome, FaPhone, FaMailBulk, FaLinkedin, FaDiscord } from "react-icons/fa"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footer-container">
+            <div className="left">
+                <div className="location">
+                    <FaHome size={22} style={{ color:"#fff", marginRight: "2rem" }} />
+                    <div>
+                        <p> Bordeaux, France.</p>
+                    </div>
+                </div>
+                <div className="phone">
+                    <h4> <FaPhone size={20} style={{ color:"#fff", marginRight: "2rem" }} /> (+33) 06.13.43.55.71 </h4>
+                </div>
+                <div className="email">
+                    <h4> <FaMailBulk size={20} style={{ color:"#fff", marginRight: "2rem" }} /> ines.bouakkaoui@gmail.com </h4>
+                </div>
+            </div>
+
+            <div className="right">
+                <h4> About my objectives </h4>
+                <p> Call me Inès! I enjoy undertaking new projects and I am always eager to learn. I have developed this website in order to share my personal self-taught projects. Do not hesitate to contact me to discuss it! </p>
+                <div className="social"> 
+                <FaLinkedin size={30} style={{ color:"#fff", marginRight: "1rem" }} />
+                <FaDiscord size={30} style={{ color:"#fff", marginRight: "1rem" }} />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import HeroImg from '../components/HeroImg'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import HeroImg from '../components/HeroImg';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <HeroImg />
+      <Footer />
     </div>
   )
 }
