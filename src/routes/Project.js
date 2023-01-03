df4a04af1d70dd10f2a9bdba5ee118b97f03f095
Ines <ines.bouakkaoui@gmail.com>
2023-01-03 23:42:14 +0100

    first step of project page

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import HeroImg2 from '../components/HeroImg2';
+
 const Project = () => {
   return (
-    <div>Project</div>
+    <div>
+      <Navbar/>
+      <HeroImg2 heading="PROJECTS." text="Some of my most recent works"/>
+      <Footer />
+    </div>
   )
 }
 
